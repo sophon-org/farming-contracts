ab55dac95318ae32578044b49f9c73ea72b5fa79
Roman Hiden <romeo8881@gmail.com>
2024-05-07 23:36:48 +0300

    created couple of deploy scripts + interfaces

---
+++ interfaces/IBridge.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+interface IBridge {
+    function deposit(
+        address _l2Receiver,
+        address _l1Token,
+        uint256 _amount,
+        uint256 _l2TxGasLimit,
+        uint256 _l2TxGasPerPubdataByte,
+        address _refundRecipient
+    ) external payable returns (bytes32 l2TxHash);
+}
