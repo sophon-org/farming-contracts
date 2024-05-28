1104d0ce6f76a79588d62e643372d8ab19213381
Roman Hiden <romeo8881@gmail.com>
2024-05-28 19:54:45 +0300

    proper transfer for stETH

---
+++ contracts/farm/interfaces/IstETH.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.25;
+
+interface IstETH {
+    function submit(address _referral) external payable returns (uint256);
+    function getSharesByPooledEth(uint256 ethAmount) external view returns (uint256);
+
+    function getPooledEthByShares(uint256 sharesAmount) external view returns (uint256);
+
+    function transferShares(address to, uint256 amount) external;
+    function transferSharesFrom(
+        address _sender,
+        address _recipient,
+        uint256 _sharesAmount
+    ) external returns (uint256);
+}
