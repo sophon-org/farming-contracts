dbc58206bb5bc2859a080ee02b069813231f8a80
mcbvictor0x <156699519+mcbvictor0x@users.noreply.github.com>
2024-05-31 13:39:11 -0400

    Update to Solidity v0.8.26; fixed versions in package.json

--- contracts/farm/SophonFarming.sol
+++ contracts/farm/SophonFarming.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity 0.8.25;
+pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
--- contracts/mocks/MockBridge.sol
+++ contracts/mocks/MockBridge.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.25;
+pragma solidity 0.8.26;
 
 import {BridgeLike} from "../farm/SophonFarmingState.sol";
 
