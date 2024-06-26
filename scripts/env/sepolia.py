e4c3f2990981ed67005d73c2a582e5ff00a20166
Roman Hiden <romeo8881@gmail.com>
2024-06-26 17:06:37 +0300

    multisig deploymetn

---
+++ scripts/env/sepolia.py
@@ -0,0 +1,9 @@
+SF = interface.ISophonFarming("0x6Af1d4F7Bd6b612B65209727417a2587A23C9264")
+# SFImpl = interface.ISophonFarming("")
+OWNER = "0x78Ae12562527B865DD1a06784a2b06dbe1A3C7AF"
+
+
+
+
+
+
