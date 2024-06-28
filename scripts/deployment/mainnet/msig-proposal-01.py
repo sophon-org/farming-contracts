3958ec00bd7d63db36e1311a3233a15f2fc7bbd6
Roman Hiden <romeo8881@gmail.com>
2024-06-28 22:03:12 +0300

    multisig proposal

--- scripts/deployment/mainnet/msig-proposal-01.py
+++ scripts/deployment/mainnet/msig-proposal-01.py
@@ -5,45 +5,50 @@ exec(open("./scripts/env/eth.py").read())
 
 safe = BrownieSafe(OWNER)
 
-USDT = safe.contract('0xdAC17F958D2ee523a2206206994597C13D831ec7')
 
-tx_list = []
+sDAI = interface.IERC20("0x83F20F44975D03b1b09e64809B757c47f942BEeA")
+USDC = interface.IERC20("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
+PEPE = interface.IERC20("0x6982508145454Ce325dDbE47a25d4ec3d2311933")
+stAethir = interface.IERC20("0xc96Aa65F31E41b4Ca6924B86D93e25686019E59C")
 
 
-tx_list.append([SF, SF.add.encode_input(10000, USDT.address, "USDT", chain.height, 0)])
+sDAIPoints = 60000
+USDCPoints = 60000
+PEPEPoints = 50000
+stAethirPoints = 40000
+
+
+sDAIEnableBlock = chain.height # TODO
+USDCEnableBlock = chain.height # TODO
+PEPEEnableBlock = chain.height # TODO
+stAethirEnableBlock = chain.height # TODO
+
+
+
+newPointsPerBlock = SF.pointsPerBlock() + (sDAIPoints+ USDCPoints + PEPEPoints + stAethirPoints) *1e14
+
+SF.set(PredefinedPool.sDAI, 60000, sDAIEnableBlock, 0, {"from": OWNER})
+SF.add(60000, USDC, "USDC", USDCEnableBlock, 0, {"from": OWNER})
+SF.add(50000, stAethir, "stAethir", PEPEEnableBlock, 0, {"from": OWNER})
+SF.add(40000, PEPE, "PEPE", stAethirEnableBlock, newPointsPerBlock, {"from": OWNER})
 
 
 safe_tx = safe.multisend_from_receipts()
 
+safe.preview(safe_tx, call_trace=True)
 
+# you need frame installed
+signtature = safe.sign_with_frame(safe_tx)
 
-
-
-
-# Post it to the transaction service
-# Prompts for a signature if needed
-for tx in tx_list:
-    sTxn = safe.build_multisig_tx(tx[0].address, 0, tx[1], SafeOperationEnum.DELEGATE_CALL.value, safe_nonce=safe.pending_nonce())
-    safe.sign_with_frame(sTxn)
-    safe.post_transaction(sTxn)
-
-# Retrieve pending transactions from the transaction service
 safe.pending_transactions
 
-# Preview the side effects of all pending transactions
+
 safe.preview_pending()
 
 
-# Execute the transactions with enough signatures
 network.priority_fee('2 gwei')
-
-# stored in .brownie/accounts/TODO.json
-signer = safe.get_signer('TODO')
+signer = safe.get_signer('any walet with eth')
 
 for tx in safe.pending_transactions:
-    # below if you have local brownie acc with private key safed. good to have one for execution only.
-    # receipt = safe.execute_transaction(safe_tx, signer)
-    
-    # or execute with frame
-    receipt = safe.execute_transaction_with_frame(tx)
+    receipt = safe.execute_transaction(safe_tx, signer)
     receipt.info()
