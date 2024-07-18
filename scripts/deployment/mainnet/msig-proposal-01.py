7bd78b7eca808b7a288038cc1a0837a199deda60
Roman Hiden <romeo8881@gmail.com>
2024-07-18 19:08:22 +0300

    updates from deployment

--- scripts/deployment/mainnet/msig-proposal-01.py
+++ scripts/deployment/mainnet/msig-proposal-01.py
@@ -1,7 +1,10 @@
 from brownie_safe import BrownieSafe
 from gnosis.safe.enums import SafeOperationEnum
+from brownie import *
 
 exec(open("./scripts/env/eth.py").read())
+
+OWNER = "0x3b181838Ae9DB831C17237FAbD7c10801Dd49fcD"
 
 safe = BrownieSafe(OWNER)
 
@@ -14,23 +17,23 @@ stAethir = interface.IERC20("0xc96Aa65F31E41b4Ca6924B86D93e25686019E59C")
 
 sDAIPoints = 60000
 USDCPoints = 60000
-PEPEPoints = 50000
-stAethirPoints = 40000
+PEPEPoints = 40000
+stAethirPoints = 50000
 
 
-sDAIEnableBlock = chain.height # TODO
-USDCEnableBlock = chain.height # TODO
-PEPEEnableBlock = chain.height # TODO
-stAethirEnableBlock = chain.height # TODO
+sDAIEnableBlock = 20269590
+USDCEnableBlock = 20269590
+PEPEEnableBlock = 20269590
+stAethirEnableBlock = 20269590
 
 
 
-newPointsPerBlock = SF.pointsPerBlock() + (sDAIPoints+ USDCPoints + PEPEPoints + stAethirPoints) *1e14
+newPointsPerBlock = SF.pointsPerBlock() + (sDAIPoints+ USDCPoints + PEPEPoints + stAethirPoints) * 1e14
 
-SF.set(PredefinedPool.sDAI, 60000, sDAIEnableBlock, 0, {"from": OWNER})
-SF.add(60000, USDC, "USDC", USDCEnableBlock, 0, {"from": OWNER})
-SF.add(50000, stAethir, "stAethir", PEPEEnableBlock, 0, {"from": OWNER})
-SF.add(40000, PEPE, "PEPE", stAethirEnableBlock, newPointsPerBlock, {"from": OWNER})
+SF.set(PredefinedPool.sDAI, sDAIPoints, sDAIEnableBlock, 0, {"from": OWNER})
+SF.add(USDCPoints, USDC, "USDC", USDCEnableBlock, 0, {"from": OWNER})
+SF.add(stAethirPoints, stAethir, "stAethir", stAethirEnableBlock, 0, {"from": OWNER})
+SF.add(PEPEPoints, PEPE, "PEPE", PEPEEnableBlock, newPointsPerBlock, {"from": OWNER})
 
 
 safe_tx = safe.multisend_from_receipts()
@@ -39,6 +42,8 @@ safe.preview(safe_tx, call_trace=True)
 
 # you need frame installed
 signtature = safe.sign_with_frame(safe_tx)
+safe.post_transaction(safe_tx)
+safe.post_signature(safe_tx, signtature)
 
 safe.pending_transactions
 
@@ -46,9 +51,10 @@ safe.pending_transactions
 safe.preview_pending()
 
 
-network.priority_fee('2 gwei')
+'''network.priority_fee('2 gwei')
 signer = safe.get_signer('any walet with eth')
 
 for tx in safe.pending_transactions:
     receipt = safe.execute_transaction(safe_tx, signer)
     receipt.info()
+'''
