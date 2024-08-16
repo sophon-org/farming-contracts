9d50dd7c30c38cefc81703182c8fc6321c252ccf
Roman Hiden <romeo8881@gmail.com>
2024-08-16 11:00:41 +0300

    deployment scripts

---
+++ scripts/deployment/mainnet/msig-proposal-03.py
@@ -0,0 +1,34 @@
+from brownie_safe import BrownieSafe
+from gnosis.safe.enums import SafeOperationEnum
+from brownie import *
+
+exec(open("./scripts/env/eth.py").read())
+
+
+# this propoposal is supposed to staked AZUR that are on the farming contract to stAZUR. it will be done in one multisig transaction
+# 1 create a small contract with only functions needed for migration
+# 2 upgrade the proxy to new contract for migration
+# 3 migrate
+# 4 upgrade the proxy to old implementation
+
+SEND_TO_MAINNET = False
+
+OWNER = "0x3b181838Ae9DB831C17237FAbD7c10801Dd49fcD"
+safe = BrownieSafe(OWNER)
+
+
+# TODO actual transaction to upgrade stAZUR
+
+
+safe_tx = safe.multisend_from_receipts()
+safe.preview(safe_tx, call_trace=True)
+
+safe.pending_transactions
+safe.preview_pending()
+
+if SEND_TO_MAINNET:
+    signtature = safe.sign_with_frame(safe_tx)
+    safe.post_transaction(safe_tx)
+    safe.post_signature(safe_tx, signtature)
+
+
