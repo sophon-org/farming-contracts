2fe1b8e38509895e6390f6498c7f7fa7a9155268
Roman Hiden <romeo8881@gmail.com>
2024-08-20 13:55:36 +0300

    merkle tree works!

--- scripts/merkle-l2/merkle-userinfo.js
+++ scripts/merkle-l2/merkle-userinfo.js
@@ -6,9 +6,9 @@ const { Web3 }= require('web3');
 // Assume this data comes from your Solidity contract
 // TODO
 const userInfos = [
-    { account: '0x0000000000000000000000000000000000000001', pid: 0, amount: BigInt(1000), boostAmount: BigInt(100), depositAmount: BigInt(900), rewardSettled: BigInt(500), rewardDebt: BigInt(200) },
-    { account: '0x0000000000000000000000000000000000000002', pid: 0, amount: BigInt(2000), boostAmount: BigInt(200), depositAmount: BigInt(1800), rewardSettled: BigInt(1000), rewardDebt: BigInt(400) },
-    { account: '0xe749b7469A9911E451600CB31B5Ca180743183cE', pid: 0, amount: BigInt(1879452157034848486000000), boostAmount: BigInt(0), depositAmount: BigInt(1879452157034848486000000), rewardSettled: BigInt(0), rewardDebt: BigInt(0) },
+    { account: '0x0000000000000000000000000000000000000001', pid: 0, amount: '1000', boostAmount: '100', depositAmount: '900', rewardSettled: '500', rewardDebt: '200' },
+    { account: '0x0000000000000000000000000000000000000002', pid: 0, amount: '2000', boostAmount: '200', depositAmount: '1800', rewardSettled: '1000', rewardDebt: '400' },
+    { account: '0xe749b7469A9911E451600CB31B5Ca180743183cE', pid: 0, amount: '1879452157034848486000000', boostAmount: '0', depositAmount: '1879452157034848486000000', rewardSettled: '0', rewardDebt: '0' },
     // Add more user info as needed
 ];
 
@@ -24,7 +24,7 @@ function hashUserInfo(userInfo) {
     );
 }
 // Create leaves by hashing each user's information
-const leaves = userInfos.map(userInfo => Buffer.from(hashUserInfo(userInfo).slice(2), 'hex'));
+const leaves = userInfos.map(userInfo => hashUserInfo(userInfo).slice(2));
 
 
 // // Create an array of hashes
@@ -46,22 +46,34 @@ const merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
 // Get the root of the tree
 const root = merkleTree.getRoot().toString('hex');
 console.log('Merkle Root:', root);
-
+console.log('leaves', leaves)
 
 // console.log(JSON.stringify(leaves))
-const proofs = merkleTree.getProofs()
 
-console.log(JSON.stringify(merkleTree.getHexProofs()))
+// console.log(JSON.stringify(proofs))
+// console.log(JSON.stringify(merkleTree.getHexProofs()))
 
-// Logging all proofs
-proofs.forEach((proof, index) => {
-    console.log("info", userInfos[index]);
-    console.log(`Proof for leaf ${index + 1}:`, proof.map(x => x.data.toString('hex')));
-});
-// // Verify the proofs
-// leaves.forEach((leaf, index) => {
-//     const proof = proofs[index];
-//     const root = merkleTree.getRoot();
-//     const isValid = merkleTree.verify(proof, leaf, root);
-//     console.log(`Proof for leaf ${index + 1} is ${isValid ? 'valid' : 'invalid'}`);
+// // Logging all proofs
+// proofs.forEach((proof, index) => {
+//     // console.log("info", userInfos[index]);
+//     console.log(`Proof for leaf ${index + 1}:`, proof.map(x => x.data.toString('hex')));
 // });
+// // Verify the proofs
+leaves.forEach((leaf, index) => {
+    const proof = merkleTree.getProof(leaf)
+    console.log("leaf", leaf)
+    console.log("p", proof.map(x => x.data.toString('hex')))
+});
+console.log("asdf")
+console.log(merkleTree.toString())
+
+
+a = Web3.utils.soliditySha3(
+    { t: 'address', v: userInfos[2].account },
+    { t: 'uint256', v: userInfos[2].pid },
+    { t: 'uint256', v: '1879452157034848486000000' },
+    // { t: 'uint256', v: userInfos[2].boostAmount.toString() },
+    // { t: 'uint256', v: userInfos[2].depositAmount.toString() }
+)
+
+console.log("a", a)
