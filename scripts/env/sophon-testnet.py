4c4ff0078a60674cb7af823330db015f0e82e1d4
Roman Hiden <romeo8881@gmail.com>
2024-12-03 17:19:49 +0200

    feat(chore): udpated contract to latest requirements

---
+++ scripts/env/sophon-testnet.py
@@ -0,0 +1,4 @@
+MA = MarkleAirdrop.at("0xAd2f55875c0768f8f9ba9c169f5A7d5D6790b7B3")
+SFIMPL = SophonFarmingL2.at("0x5701Ebf0FC165a1d5287118663f6868c4Eb0108c")
+SFProxy = SophonFarmingProxy.at("0x0fcdc6e06687756ba5aC3c3D98c1315ddFD055E9")
+SF_L2 = interface.ISophonFarming(SFProxy)
