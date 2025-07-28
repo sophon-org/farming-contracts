be6413c46515ab6577cc390552abab4710465523
Roman Hiden <romeo8881@gmail.com>
2025-07-28 19:28:15 +0300

    clean

--- scripts/deployment/mainnet/claims-L1-users.py
+++ scripts/deployment/mainnet/claims-L1-users.py
@@ -2,50 +2,50 @@ import json
 import time
 import json
 
-# MC = interface.IMerkleClaimer("0xf4551b26cbB924BFa6117aD7b5D5Da2f70Fe8b9B")
+MC = interface.IMerkleClaimer("0xf4551b26cbB924BFa6117aD7b5D5Da2f70Fe8b9B")
 
 
-# import json
+import json
 
 
-# with open("./scripts/merkle-l2/TGE/merkletree_1.json") as f1, open("./scripts/merkle-l2/TGE/merkletree_2.json") as f2:
-#     data1 = json.load(f1)
-#     data2 = json.load(f2)
-# combined = sorted(data1 + data2, key=lambda x: int(x["merkleIndex"]))
+with open("./scripts/merkle-l2/TGE/merkletree_1.json") as f1, open("./scripts/merkle-l2/TGE/merkletree_2.json") as f2:
+    data1 = json.load(f1)
+    data2 = json.load(f2)
+combined = sorted(data1 + data2, key=lambda x: int(x["merkleIndex"]))
 
-# address_to_merkle = {
-#     entry["address"].lower(): entry["merkleIndex"]
-#     for entry in combined
-# }
+address_to_merkle = {
+    entry["address"].lower(): entry["merkleIndex"]
+    for entry in combined
+}
 
-# def has_address(addr):
-#     return addr.lower() in address_to_merkle
-# def get_merkle_index(addr):
-#     return address_to_merkle.get(addr.lower())
+def has_address(addr):
+    return addr.lower() in address_to_merkle
+def get_merkle_index(addr):
+    return address_to_merkle.get(addr.lower())
 
-# unclaimed = []
+unclaimed = []
 
-# file_path = './scripts/merkle-l2/output/5-proof.json'
-# with open(file_path, 'r', encoding='utf-8') as file:
-#     data = json.load(file)
-#     claims = data.get('claims', {})
+file_path = './scripts/merkle-l2/output/5-proof.json'
+with open(file_path, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+    claims = data.get('claims', {})
  
-#     for index, user in enumerate(claims, start=1):
-#         # print(user)
-#         if has_address(user):
-#             merkleIndex = get_merkle_index(user)
-#             isClaimed = MC.isClaimed(merkleIndex)
-#             if not isClaimed:
-#                 u = {
-#                     "index": index,
-#                     "user": user,
-#                     "merkleIndex": merkleIndex
-#                 }
-#                 print(u)
-#                 unclaimed.append(u)
+    for index, user in enumerate(claims, start=1):
+        # print(user)
+        if has_address(user):
+            merkleIndex = get_merkle_index(user)
+            isClaimed = MC.isClaimed(merkleIndex)
+            if not isClaimed:
+                u = {
+                    "index": index,
+                    "user": user,
+                    "merkleIndex": merkleIndex
+                }
+                print(u)
+                unclaimed.append(u)
                 
-# with open('./scripts/merkle-l2/output/unclaimed.json', 'w', encoding='utf-8') as out:
-#     json.dump(unclaimed, out, indent=2)
+with open('./scripts/merkle-l2/output/unclaimed.json', 'w', encoding='utf-8') as out:
+    json.dump(unclaimed, out, indent=2)
 
 
 with open('./scripts/merkle-l2/output/unclaimed.json', 'r', encoding='utf-8') as f:
